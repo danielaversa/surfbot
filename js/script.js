ead252eecaf2e5394655cc11b1d9d4d6d96590fd
danielaversa <85404057+danielaversa@users.noreply.github.com>
2021-06-13 21:10:28 +0100

    Update CSS and JS
    Implements responsive layout, animations, Implements javascript for main nav

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+  var x = document.getElementById("links");
+  if (x.style.opacity === "1") {
+    x.classList.toggle("nav-open");
+  } else {
+    x.classList.toggle("nav-open");
+  }
+}
