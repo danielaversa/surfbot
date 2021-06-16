3b698a8d88a842f9fe61c5b92fa5bf9a29f087d9
danielaversa <85404057+danielaversa@users.noreply.github.com>
2021-06-16 21:02:11 +0100

    feat: Form automations
    Include changing display to selected plan and price at confirmation.

--- js/script.js
+++ js/script.js
@@ -46,6 +46,10 @@ let proBg = document.getElementById("pro-bg");
 let kid = kidBg.childNodes;
 let adult = adultBg.childNodes;
 let pro = proBg.childNodes;
+let planKid = document.getElementById("plan-kid");
+let planAdult = document.getElementById("plan-adult");
+let planPro = document.getElementById("plan-pro");
+let price = document.getElementById("confirmation-price");
 
 
 
@@ -53,34 +57,59 @@ kid[1].addEventListener("click", function() {
   kidBg.classList.add("active");
   adultBg.classList.remove("active");
   proBg.classList.remove("active");
+  planKid.classList.add("plan-active");
+  planAdult.classList.remove("plan-active");
+  planPro.classList.remove("plan-active");
+  price.innerHTML = "49,00";
 });
 
 kid[3].addEventListener("click", function() {
   kidBg.classList.add("active");
   adultBg.classList.remove("active");
   proBg.classList.remove("active");
+  planKid.classList.add("plan-active");
+  planAdult.classList.remove("plan-active");
+  planPro.classList.remove("plan-active");
+  price.innerHTML = "49,00";
 });
 
 adult[1].addEventListener("click", function() {
   adultBg.classList.add("active");
   kidBg.classList.remove("active");
   proBg.classList.remove("active");
+  planAdult.classList.add("plan-active");
+  planKid.classList.remove("plan-active");
+  planPro.classList.remove("plan-active");
+  price.innerHTML = "69,00";
 });
 
 adult[3].addEventListener("click", function() {
   adultBg.classList.add("active");
   kidBg.classList.remove("active");
   proBg.classList.remove("active");
+  planAdult.classList.add("plan-active");
+  planKid.classList.remove("plan-active");
+  planPro.classList.remove("plan-active");
+  price.innerHTML = "69,00";
 });
 
 pro[1].addEventListener("click", function() {
   proBg.classList.add("active");
   adultBg.classList.remove("active");
   kidBg.classList.remove("active");
+  planPro.classList.add("plan-active");
+  planAdult.classList.remove("plan-active");
+  planKid.classList.remove("plan-active");
+  price.innerHTML = "99,00";
 });
 
 pro[3].addEventListener("click", function() {
   proBg.classList.add("active");
   adultBg.classList.remove("active");
   kidBg.classList.remove("active");
+  planPro.classList.add("plan-active");
+  planKid.classList.remove("plan-active");
+  planAdult.classList.remove("plan-active");
+  price.innerHTML = "99,00";
 });
+
